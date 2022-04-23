6d2ccd23f830357bc237c810721b5eadd2be2e4c
Gabriel-Bento <jiggg53@gmail.com>
2022-04-23 17:31:18 -0300

    versão inicial

---
+++ src/com/softgraf/model/Cliente.java
@@ -0,0 +1,36 @@
+package com.softgraf.model;
+
+public class Cliente {
+
+	// codigo
+	private String codigo;
+	// nome
+	private String nome;
+	// enderco
+	private Endereco endereco;
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
